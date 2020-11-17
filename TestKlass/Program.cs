e8edb917c5298e57e47465f2bdae21313871b232
Sebastian <sebbebergstrom@hotmail.com>
2020-11-17 10:50:49 +0100

    Kanske är bra om jag lägger in koden jag övat på

--- TestKlass/Program.cs
+++ TestKlass/Program.cs
@@ -7,9 +7,10 @@ namespace TestKlass
 {
     class Program
     {
-        static Random generator = new Random();
+        /*static Random generator = new Random();
         static players[] cPs = new players[900];
-        public static List<retired> rPs = new List<retired>();
+        public static List<retired> rPs = new List<retired>();*/
+        static bool gameDay = true;
         static int plusDays = 0;
         //static bool isSwitch = false;
         static void printHubMenu()
@@ -19,6 +20,7 @@ namespace TestKlass
             {
                 Console.Clear();
                 DateTime dt = new DateTime(2020, 6, 1);
+                DateTime gd = new DateTime(2020, 6, 2);
                 DateTime dt1 = dt.AddDays(plusDays);
                 Console.WriteLine(dt1.ToString("D"));
                 Console.WriteLine("\n1. Simulate            2. News\n" +
@@ -30,6 +32,34 @@ namespace TestKlass
                 {
                     case "1":
                         Console.Clear();
+                        if(gameDay = dt1 == gd)
+                        {
+                            Raylib.InitWindow(1200, 750, "Court");
+                            player first = new player ("", "3", (int)100, (int) 100);
+                            player second = new player ("", "10", (int)200, (int) 300);
+                            
+                            while(!Raylib.WindowShouldClose())
+                            {
+                            Raylib.BeginDrawing();
+                            
+                            Raylib.ClearBackground(Color.WHITE);
+
+                            Raylib.DrawRectangle(0, 0, 1200, 750, Color.DARKBLUE);
+                            Raylib.DrawRectangle(150, 150, 900, 450, Color.ORANGE);
+                            Raylib.DrawRectangleLines(150, 150, 900, 450, Color.WHITE);
+                            Raylib.DrawRectangleLines(450, 150, 300, 450, Color.WHITE);
+                            Raylib.DrawLine(600, 150, 600, 600, Color.WHITE);
+                            
+                            player.DrawAll();
+                            
+                            Raylib.EndDrawing();
+                            }
+                        }
+                        else
+                        {
+                            plusDays++;
+                            break;
+                        }
                         plusDays++;
                         isValid = true;
 
